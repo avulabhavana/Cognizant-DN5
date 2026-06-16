c7e1c710ee99422046c29ea4da4e489fd4daeeb0
avulabhavana <avulabhavana226@gmail.com>
2026-06-16 21:40:57 +0530

    Add files via upload

---
+++ DEEPSKILLING/module4/Moq-Handson/TASK1/CustomerCommTests.cs
@@ -0,0 +1,35 @@
+﻿using CustomerCommLib;
+using Moq;
+using NUnit.Framework;
+
+namespace CustomerCommTests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender>? _mockMailSender;
+        private CustomerComm? _customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+
+            _mockMailSender
+                .Setup(x => x.SendMail(
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerComm(_mockMailSender.Object);
+        }
+
+        [TestCase]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            bool result = _customerComm!.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
